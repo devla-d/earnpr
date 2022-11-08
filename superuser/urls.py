2c8cfb7e194c7fdb1851ba295c1eade78edc07c3
samuel aniekan <samuelaniekan680@gmail.com>
2022-11-08 19:52:15 +0100

    modified

--- superuser/urls.py
+++ superuser/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="admindashboard"),
-    path("users/", views.users, name="users"),
+    path("investors/", views.users, name="investors"),
     path("users/<int:pk>", views.user_detail, name="user_detail"),
     path("withdrawals/", views.withdrawal_, name="withdrawal_super"),
     path("withdrawals/<int:pk>", views.withdrawal_detail, name="withdrawal_detail"),
